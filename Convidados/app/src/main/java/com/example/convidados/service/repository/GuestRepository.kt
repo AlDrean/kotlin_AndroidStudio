1f749ff81796f3916b49f45cd42b1396444cfbed
AlDrean <lucas.henrique.waki@gmail.com>
2021-12-08 22:35:04 -0300

    add insert and delete guest implemented

--- Convidados/app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ Convidados/app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,7 +1,10 @@
 package com.example.convidados.service.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.example.convidados.service.DataBaseConstants
 import com.example.convidados.service.model.GuestModel
+import java.lang.Exception
 
 class GuestRepository private constructor(context: Context){
     /**Padrão Singleton : 1 unica instância de conexão
@@ -24,10 +27,25 @@ class GuestRepository private constructor(context: Context){
     }
 
 
-    fun save(guest: GuestModel){
+    fun save(guest: GuestModel):Boolean{
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME,null,contentValues)
+            true
+
+        }catch (e:Exception){
+            false
+
+        }
+
 //        val db = GuestDataBaseHelper(context)
 //        db.writableDatabase.execSQL("") -> não segue padrões de escalabilidade
-    }
+       }
 
 
 
@@ -44,12 +62,42 @@ class GuestRepository private constructor(context: Context){
         return list
     }
 
-    fun update(){
+    fun update(guest: GuestModel):Boolean{
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            val selections = DataBaseConstants.GUEST.COLUMNS.ID+"= ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME,contentValues,selections,args)
+            true
+
+        }catch (e:Exception){
+            false
+
+        }
 
     }
 
-    fun remove(){
+    fun remove(guest: Integer):Boolean{
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
 
+            val selections = DataBaseConstants.GUEST.COLUMNS.ID+"= ?"
+            val args = arrayOf(guest)
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME,selections,args)
+
+            true
+
+        }catch (e:Exception){
+            false
+
+        }
     }
 
 }
